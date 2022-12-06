09fae50ee371c79968095c0586f076febb6db209
Emrikcah <curryandr3@gmail.com>
2022-12-06 10:11:03 -0500

    modified WriteLine statement

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World! I am learning how to use visual studio to create and update and push to github Yay!");
