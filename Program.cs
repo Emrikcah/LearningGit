3e80440d5593ac5d322ed6e66c59e00d4d1699ae
Dre <8576184+Emrikcah@users.noreply.github.com>
2022-12-06 10:14:24 -0500

    Modified WriteLine again

--- Program.cs
+++ Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! I am learning how to use visual studio to create and update and push to github Yay!");
+Console.WriteLine("Hello, World! I am learning how to use visual studio to create and update and push to github Yay! And I am really enjoying it....For the horde!");
